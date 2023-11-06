c3f30f9481516049bf84c421ed33eeb4b661fa6d
relinar <145341776+relinar@users.noreply.github.com>
2023-11-06 14:39:16 +0200

    Create korrutustabel.js

---
+++ korrutustabel.js
@@ -0,0 +1,31 @@
+SISESTADES NUMBER
+
+
+const number = parseInt(prompt("Enter an integer: "));
+
+for(let i = 1; i <= 10; i++) {
+
+    const result = i * number;
+
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
+
+
+
+TERVE KORRUTUSTABEL KUNI 10NI
+
+
+a=1
+for (let i = 1; i <= 10; i++) {
+    console.log(a)
+    
+        for(let i = 0; i <= 10; i++ ) {
+            b=a * 1
+    
+    
+    console.log(a+ "x" +i +"="+b)
+        }
+        a++
+    }
